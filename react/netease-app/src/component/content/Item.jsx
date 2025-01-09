6db69bbf84a065f17905a9d10a647927593a8ddd
haon-wuchang <haoony@naver.com>
2025-01-09 18:08:05 +0900

    Update Item.jsx

--- react/netease-app/src/component/content/Item.jsx
+++ react/netease-app/src/component/content/Item.jsx
@@ -3,8 +3,24 @@ import React from 'react';
 export default function Item() {
     return (
         <div>
-            
+             const itemList = [ 
+                {
+                "img":"https://a0.muscache.com/im/pictures/prohost-api/Hosting-696191916548468403/original/88873c6c-3809-42e2-9e65-9a6d879325d5.jpeg?im_w=720&im_format=avif",
+                "title":"선지자 피규어",
+                "price":"14.000",
+                "sale": true,                
+                },
+                   {
+                "img":"https://a0.muscache.com/im/pictures/prohost-api/Hosting-696191916548468403/original/88873c6c-3809-42e2-9e65-9a6d879325d5.jpeg?im_w=720&im_format=avif",
+                "title":"선지자 피규어",
+                "price":"14.000",
+                "sale": true,                
+                },
+
+            ]
         </div>
     );
 }
-
+// 이런식으로 만들면 댕
+// sale 이 true 이면 할인된 가격까지 보이게 하는거임 이거 css도 우선 하나 만들어놔봐
+// {props.sale && <span class='sale--price'> 10,000 </span>} 
